5ac8f7db4e697706f36a44d592337693b8a39c74
Petro Matiushko <72012894+OHPetro@users.noreply.github.com>
2023-04-20 00:07:56 +0200

    Update lab1.R

--- DA_lab1/lab1.R
+++ DA_lab1/lab1.R
@@ -1,17 +1,26 @@
-library(psych)
+library(psych) 
 
+#  дата считваем 
+df <- read.csv("")
+
+#общее количество пропусков 
 sum(is.na(df))
-sum(is.na(df$totalRent))
-sum(is.na(df$heatingCosts))
 
-
+# общее сведения
 str(df)
 summary(df)
 
+# делаем новую таблицу с теми столбцами что нам подходят
 new_df <- subset(df, select =  c(regio1, regio2, baseRentRange, geo_plz , serviceCharge, yearConstructed, heatingType, telekomTvOffer, newlyConst, balcony, picturecount, pricetrend, totalRent, scoutId, noParkSpaces , firingTypes, hasKitchen, cellar, houseNumber, livingSpace, geo_krs, condition, petsAllowed, lift, typeOfFlat, noRooms, floor, numberOfFloors, noRoomsRange, garden, heatingCosts, energyEfficiencyClass, lastRefurbish, date))
 View(new_df)
 
+# общее сведения
 str(new_df)
 summary(new_df)
 
+# общее количество пропусков по колонкам
 sapply(new_df, function (x) sum(is.na (x)))
+
+
+
+
